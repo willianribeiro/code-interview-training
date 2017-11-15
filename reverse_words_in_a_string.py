e45802497ae8684b8db475170496ca8ad0233aa5
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-11-15 12:48:41 -0200

    Add solution for reverse words in a string (python)

---
+++ reverse_words_in_a_string.py
@@ -0,0 +1,10 @@
+# Exercise: https://www.programcreek.com/2014/05/leetcode-reverse-words-in-a-string-ii-java/
+#
+# For example:
+# Given s = "the sky is blue",
+# return "blue is sky the".
+
+str = 'the sky is blue'
+lst = str.split()
+lst.reverse()
+print lst
